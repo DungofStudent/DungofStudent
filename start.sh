977d3a2528d1205080e232a9dcfdb386d2547253
DungofStudent <174160269+DungofStudent@users.noreply.github.com>
2025-09-04 07:16:17 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+set -e
+
+# update pip và wheel để chắc chắn không bị lỗi biên dịch
 pip install --upgrade pip setuptools wheel
+
+# cài dependencies
 pip install -r requirements.txt
+
+# chạy bot
 exec python crypto_research_bot_final.py

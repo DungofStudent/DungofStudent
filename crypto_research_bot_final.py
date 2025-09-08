82168b07b68c24f548e91fb7c5cd4361f6395d7b
DungofStudent <174160269+DungofStudent@users.noreply.github.com>
2025-09-08 18:36:06 +0700

    Update crypto_research_bot_final.py

--- crypto_research_bot_final.py
+++ crypto_research_bot_final.py
@@ -2170,9 +2170,8 @@ def main():
 
     # Handlers
     application.add_handler(CommandHandler("start", start_handler))
-	
     application.add_handler(CommandHandler("start", start_command))
-	application.add_handler(CommandHandler("research", research_command))
+    application.add_handler(CommandHandler("research", research_command))
     application.add_handler(CommandHandler("research", research_handler))
     application.add_handler(CommandHandler("deepcoin", deepcoin_handler))
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, text_coin_handler))
